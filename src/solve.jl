8f75c2302b2800bdd7ac74619287424ad7d64dba
syyui <syyui@syyuideMacBook-Pro.local>
2025-05-31 17:54:17 +0800

    how to better precisely get poles?

--- src/solve.jl
+++ src/solve.jl
@@ -5,17 +5,19 @@ struct CtxData{T<:Real}
     iwn::Vector{Complex{T}}
     mesh::Vector{T}
     mesh_weights::Vector{T}
+    η::T
     function CtxData(
         β::T,
         N::Int;
         mesh_bound = ACFSDefults.mesh_bound[]::Real,
         mesh_length = ACFSDefults.mesh_length[]::Int,
         mesh_type::Mesh = ACFSDefults.mesh_type[]::Mesh,
+        η::T = T(1e-2),
     ) where {T<:Real}
         wn = (collect(0:(N-1)) .+ T(0.5)) * T(2π) / β
         iwn = (collect(0:(N-1)) .+ T(0.5)) * T(2π) / β * im
         mesh, mesh_weights = make_mesh(T(mesh_bound), mesh_length, mesh_type)
-        return new{T}(β, N, wn, iwn, mesh, mesh_weights)
+        return new{T}(β, N, wn, iwn, mesh, mesh_weights, η)
     end
 end
 
